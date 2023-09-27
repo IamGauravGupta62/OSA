e52c95153a43a43caca32a71b5e2c4134883bd12
mayur <misampelliwar@gmail.com>
2023-09-27 17:24:36 +0530

    insert prosuctclass commit

---
+++ src/test/java/com/practice/InsertProductTest.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class InsertProductTest {
+	@Test
+	public void insertProduct() {
+		System.out.println("Product has been insserted");
+	}
+}
