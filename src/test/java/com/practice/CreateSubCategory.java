20f69c131cb7fe08c4e65988499592e131c7cf0a
mayur <misampelliwar@gmail.com>
2023-09-28 11:43:28 +0530

    Commit For CreateSubCategory

---
+++ src/test/java/com/practice/CreateSubCategory.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class CreateSubCategory {
+	@Test
+	public void createSC() {
+		System.out.println("As User Created the SubCategory");
+	}
+}
