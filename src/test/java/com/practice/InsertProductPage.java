0a0c640fb2f193e3fb4e44fed376944d38abe22c
mayur <misampelliwar@gmail.com>
2023-09-28 09:57:14 +0530

    insert product page commit

---
+++ src/test/java/com/practice/InsertProductPage.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class InsertProductPage {
+	@Test
+	public void insert() {
+		System.out.println("Insert the product is done");
+	}
+}
